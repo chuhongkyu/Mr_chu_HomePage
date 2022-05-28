8852fe850fff8286da68c01b1befe89ce213e592
Mr.chu <chuhongkyu@gmail.com>
2022-05-28 14:47:44 +0900

    npm 문제 발생

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,62 +7,37 @@ env.PUBLIC_URL = env.PUBLIC_URL || "";
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
-  position: relative;
-  display: flex;
-`;
-
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  position: absolute;
-  z-index: 2;
 `;
 
 const Window = styled.div`
   width: 100%;
-  height: 80vh;
+  height: 95vh;
   background-color: ${(props) => props.theme.glass};
 `;
 
-const Panel = styled.div`
-  width: 30vw;
-  height: 80vh;
-  background-image: url("${env.PUBLIC_URL}/assets/img/photo1.jpg");
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  &:nth-of-type(2) {
-    background-image: url("${env.PUBLIC_URL}/assets/img/photo3.jpg");
-  }
-  &:last-of-type {
-    background-image: url("${env.PUBLIC_URL}/assets/img/photo2.jpg");
-  }
+const Container = styled.div`
+  width: 20vw;
+  height: 90vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const DownPlace = styled.div`
+const WindowBar = styled.div`
   width: 100%;
-  height: 20vh;
-  background-color: ${(props) => props.theme.brown.darker};
-  div {
-    height: 20px;
-    background-color: ${(props) => props.theme.brown.lighter};
-  }
+  height: 5vh;
+  background-color: ${(props) => props.theme.black.darker};
 `;
 
 const Home = () => {
   return (
     <Wrapper>
-      <Container>
-        <Window>
+      <Window>
+        <Container>
           <AppLink title="App" img={env.PUBLIC_URL + "assets/img/photo1.jpg"} />
-        </Window>
-        <DownPlace>
-          <div></div>
-        </DownPlace>
-      </Container>
-      <Panel></Panel>
-      <Panel></Panel>
-      <Panel></Panel>
+        </Container>
+      </Window>
+      <WindowBar></WindowBar>
     </Wrapper>
   );
 };
