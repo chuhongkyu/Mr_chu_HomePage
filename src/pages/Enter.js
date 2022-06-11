43adb0c055a497b3b5161cc5fb8577a578cf8765
Mr.chu <chuhongkyu@gmail.com>
2022-06-11 21:52:16 +0900

    enter 애니메이션

--- src/pages/Enter.js
+++ src/pages/Enter.js
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Title from "../components/Title";
 
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+
 const Wrapper = styled(motion.section)`
   width: 100%;
   height: 100vh;
@@ -24,18 +27,16 @@ const LoginBox = styled.div`
     align-items: center;
     flex-direction: column;
   }
-  h1 {
-    font-size: 40px;
-    font-weight: 800;
-    color: white;
-  }
 `;
 
-const Circle = styled.div`
+const Circle = styled(motion.div)`
   width: 100px;
   height: 100px;
   border-radius: 50%;
-  background-color: white;
+  background-image: url("${env.PUBLIC_URL}/assets/img/mrchu.jpeg");
+  background-position: center;
+  background-size: cover;
+  border: 2px solid white;
 `;
 
 const openVariants = {
@@ -55,8 +56,7 @@ const Enter = () => {
     <Wrapper variants={openVariants} initial="inital" animate="animate">
       <LoginBox>
         <Link to="home">
-          <Circle></Circle>
-          <h1>Mr_chu</h1>
+          <Circle whileHover={{ y: -5 }}></Circle>
           <Title />
         </Link>
       </LoginBox>
