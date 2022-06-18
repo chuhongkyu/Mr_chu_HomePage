ffa2bf9ad8ff99751c03b5aee0bc6fac1e4cc8f0
Mr.chu <chuhongkyu@gmail.com>
2022-06-18 10:00:37 +0900

    data_arrangement2

--- src/pages/About.js
+++ src/pages/About.js
@@ -98,7 +98,7 @@ const About = () => {
           </p>
           <MainText>
             <h5 name={"firstCode"} onClick={onClick} value={firstCode}>
-              <span>📌</span> {write.about.main_txt}
+              <span>📌</span> {write.about[0].main_txt}
             </h5>
             {open.firstCode ? (
               <SubText
@@ -107,20 +107,20 @@ const About = () => {
                 transition={{ duration: 0.5 }}
               >
                 <div>
-                  <p>{write.about.sub_txt1}</p>
+                  <p>{write.about[0].sub_txt1}</p>
                 </div>
                 <div>
-                  <p>{write.about.sub_txt2}</p>
+                  <p>{write.about[0].sub_txt2}</p>
                 </div>
                 <div>
-                  <p>{write.about.sub_txt3}</p>
+                  <p>{write.about[0].sub_txt3}</p>
                 </div>
               </SubText>
             ) : null}
           </MainText>
           <MainText>
             <h5 name={"codeFeel"} onClick={onClick} value={codeFeel}>
-              <span>📌</span> 진지하게 개발 공부를 하며 부딪힌 벽
+              <span>📌</span> {write.about[1].main_txt}
             </h5>
             {open.codeFeel ? (
               <SubText
@@ -129,24 +129,14 @@ const About = () => {
                 transition={{ duration: 0.5 }}
               >
                 <div>
-                  <p>
-                    개발 공부를 처음 본격적으로 배우기 시작한 것은 2021년 노마드
-                    코드 인터넷 강의를 통해서 입니다. html,css,바닐라
-                    자바스크립트, scss 순으로 챌린지를 진행하며 통과하였습니다.
-                    사실 홈페이지를 디자인만하는 정도인 것이지만 스스로 코딩을
-                    잘한다고 생각하며 신나 있었습니다. 그러다 리액트 챌린지를
-                    시작하고 알게 되었습니다. 자바스크립트에 대해 전혀 모르고
-                    있었다는 것을... 저는 처음으로 챌린지를 포기하고 말았습니다.
-                    그리고 '내가 개발자를 할 수 있는 것일까?' 생각하게
-                    되었습니다.
-                  </p>
+                  <p>{write.about[1].sub_txt1}</p>
                 </div>
               </SubText>
             ) : null}
           </MainText>
           <MainText>
             <h5 name={"nowCode"} onClick={onClick} value={nowCode}>
-              <span>📌</span> 개발 일을 하며 내가 느낀점
+              <span>📌</span> {write.about[2].main_txt}
             </h5>
             {open.nowCode ? (
               <SubText
@@ -155,47 +145,20 @@ const About = () => {
                 transition={{ duration: 0.5 }}
               >
                 <div>
-                  <h2>전반적인 업무</h2>
-                  <p>
-                    2022년 마포구 일자리 사업단 앱 개발팀에서 일하게 되었습니다.
-                    주요 업무는 마포구에서 마포구 캐릭터를 제작하는데 도움을
-                    주는 것이였습니다.
-                  </p>
+                  <h2>{write.about[2].sub_title1}</h2>
+                  <p>{write.about[2].sub_txt1}</p>
                 </div>
                 <div>
-                  <h2>회사를 다니며 js 공부를 한 방법</h2>
-                  <p>
-                    저희 팀에는 14년차 개발자 PM님이 계셨습니다. PM님은 저희가
-                    많이 부족하여 매일 1시간씩 자바스립트에 대한 연습과 문제를
-                    내 주셨습니다. 비전공자인 저는 스스로 많이 부족하다고 느꼈고
-                    열심히 예습, 복습을 하였습니다. 어느 순간 작년에 공부하던
-                    js기반 프로젝트들도 이해가 되었고 스스로
-                    자바스크립트만으로는 모든 만들수 있다는 자신감을 얻게
-                    되었습니다.
-                  </p>
+                  <h2>{write.about[2].sub_title2}</h2>
+                  <p>{write.about[2].sub_txt2}</p>
                 </div>
                 <div>
-                  <h2>자바스크립트를 이해한 후 다시 해본 리액트</h2>
-                  <p>
-                    노마드코더 리액트 마스터 클래스를 다시 챌린지 했습니다. 퇴근
-                    후 공부하는 것은 쉽지 않았지만 작년에는 이해가 안되었던
-                    것들이 이해가 되어 너무 재미가 있었습니다. 다행히도 무사히
-                    챌린지에 합격하였습니다.
-                  </p>
+                  <h2>{write.about[2].sub_title3}</h2>
+                  <p>{write.about[2].sub_txt3}</p>
                 </div>
                 <div>
-                  <h2>비전공자로서 부족한 점, 해결하기 위한 노력</h2>
-                  <p>
-                    확실히 현대미술을 해왔던 저는 디자인 적인 부분이나 만드는
-                    부분에서 개발 이해가 쉬웠습니다. 모달 창이나 슬라이드
-                    컨테이너를 만들어도 다른 팀원들에 비해 어떻게 하면 유저에게
-                    좋은 경험을 남길수 있을지 본능적으로 알고 있었던 것
-                    같습니다. 하지만 전공자들에 비해 컴퓨터 지식, 웹엔진,
-                    자바스크립트 원리에 대해 떨어진다고 생각하게 되었습니다.
-                    회사에 가면 상사가 일을 시켜도 시킨 일이 뭔지 몰라서 일을
-                    못한다는 말을 들었습니다. 이것을 해결하기 위해 노력을 많이
-                    하고 지금도 하는 중일 것입니다.
-                  </p>
+                  <h2>{write.about[2].sub_title4}</h2>
+                  <p>{write.about[2].sub_txt4}</p>
                   <div>
                     <p>
                       'IT용어 정리' 지금의 회사에 들어와서 처음 했던 일 입니다.
