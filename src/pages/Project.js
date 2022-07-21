9a6f25b7903665581db4b1fcfd9e04ed98547524
Mr.chu <chuhongkyu@gmail.com>
2022-07-22 00:29:46 +0900

    자료 업데이트

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -21,10 +21,13 @@ const Title = styled.h1`
   white-space: nowrap;
   font-size: 35px;
   font-weight: 800;
-  margin-bottom: 20px;
+  margin-bottom: 2rem;
   img {
     width: 25px;
     margin-right: 10px;
+  }
+  @media ${(props) => props.theme.device.mac} {
+    margin-bottom: 1rem;
   }
 `;
 
@@ -41,6 +44,9 @@ const Modal = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  @media ${(props) => props.theme.device.mac} {
+    height: 500px;
+  }
 `;
 
 const Works = styled(motion.div)`
@@ -76,19 +82,38 @@ const Works = styled(motion.div)`
       margin-bottom: 40px;
       text-align: end;
     }
+    h3 {
+      font-size: 25px;
+    }
     img {
       width: 100%;
       height: auto;
       border: 2px solid black;
     }
+    hr {
+      margin-top: 5px;
+      margin-bottom: 5px;
+    }
     p {
     }
     table {
       width: 100%;
+      white-space: nowrap;
       th {
         text-align: start;
         font-size: 20px;
         font-weight: 600;
+      }
+    }
+  }
+  @media ${(props) => props.theme.device.mac} {
+    height: 500px;
+    .container {
+      h1 {
+        margin-bottom: 5px;
+      }
+      h6 {
+        margin-bottom: 10px;
       }
     }
   }
@@ -113,6 +138,10 @@ const CircleContainer = styled(motion.div)`
   overflow: hidden;
   position: relative;
   box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  @media ${(props) => props.theme.device.mac} {
+    width: 400px;
+    height: 400px;
+  }
 `;
 
 const SmallContainer = styled(motion.div)`
@@ -139,6 +168,14 @@ const SmallContainer = styled(motion.div)`
       background: linear-gradient(to left, #d5d6d7 50%, transparent 50%);
     }
   }
+  @media ${(props) => props.theme.device.mac} {
+    width: 300px;
+    height: 300px;
+    .line {
+      width: 300px;
+      height: 300px;
+    }
+  }
 `;
 
 const PathPosition = styled.div`
@@ -154,11 +191,41 @@ const PathPosition = styled.div`
     color: black;
     transform: translateY(-170px);
   }
+  @media ${(props) => props.theme.device.mac} {
+    h5 {
+      color: black;
+      transform: translateY(-200px);
+    }
+  }
 `;
 
 const worksData = [
   {
     id: 0,
+    name: "헌 옷 수거 플랫폼",
+    date: "(2022.07)~ 현재 진행형",
+    img: "/assets/works/3project.png",
+    description:
+      "옷을 수거 하여 리세일하는 플랫폼입니다. 프론트엔드 리드로서 환경설정, 로그인 페이지, 배너를 현재 구현 했습니다. 백에서 JWT를 받아와 쿠키에 refreshToken을 저장하고 Redux_toolkit을 활용하여 Store에 accessToken을 전역으로 관리합니다. ",
+    point: ["로그인, 판매, 예약"],
+    github: "https://github.com/Mapo-Project/SecondLife-frontend",
+    skills: ["React"],
+    people: "UI/UX 2명, 프론트 엔드 3명, 백엔드 1명",
+  },
+  {
+    id: 1,
+    name: "마포 버디즈",
+    date: "2022.06 ~ 2022.07",
+    img: "/assets/works/2project.png",
+    description:
+      "마포 버디즈 홈페이지, 버디즈는 우체통 캐릭터입니다. Email.js를 활용하여 닉네임과 Email을 입력시 행운의 편지가 메일로 전송됩니다. 귀여운 캐릭터와 일러스트를 스크롤 홈페이지로 감상이 가능합니다. 2022년 캐릭터 라이선싱 페어 전시 되었습니다.",
+    point: ["상세 페이지", "Email 송신 시스템"],
+    github: "https://chuhongkyu.github.io/mapoCharacter/",
+    skills: ["React"],
+    people: "캐릭터 디자인 5명, 프론트 엔드(1인 개발)",
+  },
+  {
+    id: 2,
     name: "인터랙티브 코딩 취미",
     date: "(2022.03)",
     img: "/assets/works/cube.jpg",
@@ -170,19 +237,19 @@ const worksData = [
     people: "개인 프로젝트",
   },
   {
-    id: 1,
+    id: 3,
     name: "BMW",
     date: "2022.04",
     img: "/assets/works/bmw.jpg",
     description:
-      "리액트와 Three 라이브러리를 활용하여 BMW모델을 인터랙티브하게 감상할 수 있게 하였다. ",
+      "리액트와 Three 라이브러리를 활용하여 BMW모델을 인터랙티브하게 감상할 수 있게 하였습니다. 차체, 창문, 바퀴 색을 직접 변경하며 감상할 수 있습니다",
     point: ["모델 파츠 별로 색깔 변경 기능", "모델 회전"],
     github: "https://chuhongkyu.github.io/bmw-car/",
     skills: ["React", "three 라이브러리", "Blender"],
     people: "개인 프로젝트",
   },
   {
-    id: 2,
+    id: 4,
     name: "넷플릭스",
     date: "2022.05",
     img: "/assets/works/chuflix.jpg",
@@ -193,7 +260,7 @@ const worksData = [
     people: "(개인 프로젝트)",
   },
   {
-    id: 3,
+    id: 5,
     name: "댓글앱",
     date: "2022.05",
     img: "/assets/works/comments.jpg",
@@ -204,41 +271,22 @@ const worksData = [
     people: "(개인 프로젝트)",
   },
   {
-    id: 4,
-    name: "댓글앱",
-    date: "2022.06",
-    img: "/assets/works/comments.jpg",
-    description: "리액트로 만든 댓글 앱",
-    point: ["댓글 작성, 수정, 삭제, 체크"],
-    github: "https://chuhongkyu.github.io/Comments_app/",
-    skills: ["React"],
-    people: "(개인 프로젝트)",
-  },
-  {
-    id: 5,
-    name: "댓글앱",
-    date: "2022.06",
-    people: "2022.03 (개인 프로젝트)",
-    img: "/assets/works/comments.jpg",
-    description: "리액트로 만든 댓글 앱",
-    point: ["댓글 작성, 수정, 삭제, 체크"],
-    github: "https://chuhongkyu.github.io/Comments_app/",
-    skills: ["React"],
-  },
-  {
     id: 6,
-    name: "댓글앱",
-    date: "2022.06",
-    img: "/assets/works/comments.jpg",
-    description: "리액트로 만든 댓글 앱",
-    point: ["댓글 작성, 수정, 삭제, 체크"],
-    github: "https://chuhongkyu.github.io/Comments_app/",
-    skills: ["React"],
-    people: "(개인 프로젝트)",
+    name: "마포구 이쁜 카페 소개 10선",
+    date: "2022.05",
+    img: "/assets/works/1project.png",
+    description:
+      "마포구 이쁜 카페 소개 10선, 코로나로 인해 경기 침체가 장기화되어 왔다. 그러나 이제는 거리 두기 제한이 풀렸다. 또한 실외 마스크 착용이 의무가 아니게 되었다. 여름을 맞이하여 마포구의 이동과 소비가 늘어날 것이다. 우리 마포구 또한 이러한 상황에 맞게 경제 활성화를 다시 일으켜야 한다. 우리나라에 커피 문화는 굉장히 크게 자리 잡고 있다. 바쁜 직장인들이 잠을 이겨내기 위해 마시는 것에서 부터 주말에 데이트를 하러 가서 까지 소비하고 있다. 현대인들은 어디에 놀러 가든지 카페에서 커피를 마실 시간을 필요로 하게 되었다. 이러한 사람들의 수요에 맞게 마포구에는 굉장히 이쁜 카페들이 줄줄이 생겨가고 있다. 마포구내 카페를 활용하여 유동인구를 늘리고 경제를 활성화 시켜보자. 마포구에 이쁜 카페에 놀러온 사람들은 커피만 마시고 떠나지 않는다. 주변에서 식사도 하고 구경을 하며 지역을 발전 시킬 것이다. ",
+    point: ["카페 검색, 지도, 인터랙티브, 다크모드"],
+    github: "https://chuhongkyu.github.io/Cafe_HomePage/",
+    skills: ["javaScript"],
+    people: "1인 개발 - 회사 프로젝트",
   },
   {
     id: 7,
     name: "댓글앱",
+    date: "2022.06",
+    people: "2022.03 (개인 프로젝트)",
     img: "/assets/works/comments.jpg",
     description: "리액트로 만든 댓글 앱",
     point: ["댓글 작성, 수정, 삭제, 체크"],
@@ -251,8 +299,8 @@ const worksData = [
     date: "2022.06 ~ 2022.07",
     img: "/assets/works/2project.png",
     description: "마포 버디즈 홈페이지",
-    point: ["영화,드라마 검색", "콘텐츠 상세 보기"],
-    github: "https://chuhongkyu.github.io/chuflix/",
+    point: ["상세 페이지", "Email.js"],
+    github: "https://chuhongkyu.github.io/mapoCharacter/",
     skills: ["React"],
     people: "캐릭터 디자인 5명, 프론트 엔드(1인 개발)",
   },
@@ -303,6 +351,7 @@ const Project = () => {
                     </div>
                     <div className="container">
                       <p>{data.description}</p>
+                      <hr />
                       <table>
                         <tr>
                           <th>
@@ -341,6 +390,15 @@ const Project = () => {
                                 <p key={index}>{m}</p>
                               ))}
                             </tr>
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>
+                            <AiTwotoneCrown style={{ marginRight: 5 }} />
+                            개발 :
+                          </th>
+                          <td>
+                            <tr>{data.people}</tr>
                           </td>
                         </tr>
                       </table>
