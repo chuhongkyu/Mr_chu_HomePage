6bd590af6a9891338b218a4fae49a910b1032c53
Mr.chu <chuhongkyu@gmail.com>
2022-07-21 15:24:46 +0900

    자료 추가

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import WindowModal from "../components/WindowModal";
 import { motion } from "framer-motion";
+import { AiTwotoneCrown } from "react-icons/ai";
 
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || "";
@@ -20,6 +21,11 @@ const Title = styled.h1`
   white-space: nowrap;
   font-size: 35px;
   font-weight: 800;
+  margin-bottom: 20px;
+  img {
+    width: 25px;
+    margin-right: 10px;
+  }
 `;
 
 const MyStory = styled.div`
@@ -38,24 +44,43 @@ const Modal = styled(motion.div)`
 `;
 
 const Works = styled(motion.div)`
-  width: 50vw;
+  width: 100%;
   height: 700px;
   background-color: white;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   border-radius: 15px;
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: flex-start;
-  flex-direction: column;
-  padding: 20px;
-  h1 {
-    font-size: 25px;
-  }
-  img {
-    width: 500px;
-  }
-  p {
+  padding: 3rem;
+  .container {
+    width: 50%;
+    display: flex;
+    justify-content: flex-start;
+    align-content: center;
+    flex-direction: column;
+    margin-right: 50px;
+    h1 {
+      font-size: 30px;
+      font-weight: 700;
+      margin-bottom: 10px;
+    }
+    img {
+      width: 100%;
+      height: auto;
+      border: 2px solid black;
+    }
+    p {
+    }
+    table {
+      width: 100%;
+      th {
+        text-align: start;
+        font-size: 20px;
+        font-weight: 600;
+      }
+    }
   }
 `;
 
@@ -85,7 +110,7 @@ const SmallContainer = styled(motion.div)`
   height: 300px;
   background-color: #eef2f5;
   border-radius: 50%;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -128,6 +153,9 @@ const worksData = [
     name: "인터랙티브 코딩 취미",
     img: "/assets/works/cube.jpg",
     description: "자바스크립트와 CSS만으로 만든 인터랙티브 작업",
+    point: ["마우스에 따라 돌아다니는 고스트", "회전하는 3D박스"],
+    github: "https://chuhongkyu.github.io/Workspace_MaPo/",
+    skills: ["바닐라 JS", "Html,css"],
   },
   {
     id: 1,
@@ -135,48 +163,72 @@ const worksData = [
     img: "/assets/works/bmw.jpg",
     description:
       "리액트와 Three 라이브러리를 활용하여 BMW모델을 인터랙티브하게 감상할 수 있게 하였다. ",
+    point: ["모델 파츠 별로 색깔 변경 기능", "모델 회전"],
+    github: "https://chuhongkyu.github.io/bmw-car/",
+    skills: ["React", "three 라이브러리", "Blender"],
   },
   {
     id: 2,
     name: "넷플릭스",
     img: "/assets/works/chuflix.jpg",
     description: "리액트와 타입스크립트를 활용한 넷플릭스 클론코딩",
+    point: ["영화,드라마 검색", "콘텐츠 상세 보기"],
+    github: "https://chuhongkyu.github.io/chuflix/",
+    skills: ["React", "TypeScript"],
   },
   {
     id: 3,
     name: "댓글앱",
     img: "/assets/works/댓글.png",
-    description: "리액트와 타입스크립트를 활용한 넷플릭스 클론코딩",
+    description: "리액트로 만든 댓글 앱",
+    point: ["댓글 작성, 수정, 삭제, 체크"],
+    github: "https://chuhongkyu.github.io/Comments_app/",
+    skills: ["React"],
   },
   {
     id: 4,
     name: "댓글앱",
     img: "/assets/works/댓글.png",
-    description: "리액트와 타입스크립트를 활용한 넷플릭스 클론코딩",
+    description: "리액트로 만든 댓글 앱",
+    point: ["댓글 작성, 수정, 삭제, 체크"],
+    github: "https://chuhongkyu.github.io/Comments_app/",
+    skills: ["React"],
   },
   {
     id: 5,
     name: "댓글앱",
     img: "/assets/works/댓글.png",
-    description: "리액트와 타입스크립트를 활용한 넷플릭스 클론코딩",
+    description: "리액트로 만든 댓글 앱",
+    point: ["댓글 작성, 수정, 삭제, 체크"],
+    github: "https://chuhongkyu.github.io/Comments_app/",
+    skills: ["React"],
   },
   {
     id: 6,
     name: "댓글앱",
     img: "/assets/works/댓글.png",
-    description: "리액트와 타입스크립트를 활용한 넷플릭스 클론코딩",
+    description: "리액트로 만든 댓글 앱",
+    point: ["댓글 작성, 수정, 삭제, 체크"],
+    github: "https://chuhongkyu.github.io/Comments_app/",
+    skills: ["React"],
   },
   {
     id: 7,
     name: "댓글앱",
     img: "/assets/works/댓글.png",
-    description: "리액트와 타입스크립트를 활용한 넷플릭스 클론코딩",
+    description: "리액트로 만든 댓글 앱",
+    point: ["댓글 작성, 수정, 삭제, 체크"],
+    github: "https://chuhongkyu.github.io/Comments_app/",
+    skills: ["React"],
   },
   {
     id: 8,
     name: "마포 버디즈",
     img: "/assets/works/2project.png",
     description: "마포 버디즈 홈페이지",
+    point: ["영화,드라마 검색", "콘텐츠 상세 보기"],
+    github: "https://chuhongkyu.github.io/chuflix/",
+    skills: ["React"],
   },
 ];
 
@@ -195,24 +247,77 @@ const Project = () => {
     <WindowModal bgColor="white">
       <MainContainer>
         <MyStory>
-          <Title>MY PROJECT</Title>
+          <Title>
+            <img
+              src="https://notion-emojis.s3-us-west-2.amazonaws.com/prod/svg-twitter/1f4a1.svg"
+              alt="아이디어"
+            />
+            MY PROJECT
+          </Title>
           <Modal>
             {worksData.map((data) => (
               <>
                 {position === data.id ? (
                   <Works
-                    initial={{ x: 0, opacity: 1 }}
+                    initial={{ opacity: 1 }}
                     animate={{
-                      x: [-10, 0],
+                      y: [500, 0],
+                      x: [-500, 0],
+                      rotate: [-80, 0],
                       opacity: [0, 1],
-                      transition: { duration: 0.5 },
+                      transition: { duration: 0.5, type: "spring" },
                     }}
                     key={data.id}
                     id={data.id}
                   >
-                    <h1>{data.name}</h1>
-                    <img src={env.PUBLIC_URL + data.img} alt={data.name} />
-                    <p>{data.description}</p>
+                    <div className="container">
+                      <h1>{data.name}</h1>
+                      <img src={env.PUBLIC_URL + data.img} alt={data.name} />
+                      <p>{data.description}</p>
+                    </div>
+                    <div className="container">
+                      <table>
+                        <tr>
+                          <th>
+                            <AiTwotoneCrown style={{ marginRight: 5 }} />
+                            주요 기능 :
+                          </th>
+                          <td>
+                            {data.point.map((m, index) => (
+                              <p key={index}>{m}</p>
+                            ))}
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>
+                            <AiTwotoneCrown style={{ marginRight: 5 }} />깃 허브
+                            :
+                          </th>
+                          <td>
+                            <a
+                              href={data.github}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              {data.github}
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <th>
+                            <AiTwotoneCrown style={{ marginRight: 5 }} />
+                            기술 :
+                          </th>
+                          <td>
+                            <tr>
+                              {data.skills.map((m, index) => (
+                                <p key={index}>{m}</p>
+                              ))}
+                            </tr>
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
                   </Works>
                 ) : null}
               </>
