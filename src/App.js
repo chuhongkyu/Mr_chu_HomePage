09ed8c3b355a3d7452104946715fd9232619c16f
Mr.chu <chuhongkyu@gmail.com>
2022-08-17 15:44:43 +0900

    리액트 헬멧 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Enter from "./pages/Enter";
 import Home from "./pages/Home";
 
 function App() {
+  let params = useParams();
+  useEffect(() => {
+    console.log(params);
+  }, []);
+
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Enter />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/home/*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <Helmet>
+        <title>Mr.chu 홈페이지</title>
+        <meta name="미스터 추" content="미스터 추" />
+      </Helmet>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Enter />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home/*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
