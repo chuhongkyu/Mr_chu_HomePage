d4774ef428b00a0b032d8fe56eec19fea87661aa
Mr.chu <chuhongkyu@gmail.com>
2022-08-18 20:07:07 +0900

    패스

--- src/utils/Meta.js
+++ src/utils/Meta.js
@@ -7,6 +7,9 @@ const Meta = () => {
     title: "Mr.chu 홈페이지",
     description: "Mr.chu 홈페이지 - 개발자, 포트폴리오, 프론트엔드",
     keywords: "개발자, 프론트 엔드, 미스터추, 포트폴리오",
+    imgsrc:
+      "https://raw.githubusercontent.com/chuhongkyu/Mr_chu_HomePage/main/public/assets/home.png",
+    url: "https://chuhongkyu.github.io/Mr_chu_HomePage/",
   };
   return (
     <Helmet>
@@ -19,12 +22,12 @@ const Meta = () => {
       <meta property="og:title" content={metaData.title} />
       <meta property="og:site_name" content={metaData.title} />
       <meta property="og:description" content={metaData.description} />
-      {/* <meta property="og:image" content={metaData.imgsrc} />
-      <meta property="og:url" content={metaData.url} /> */}
+      <meta property="og:image" content={metaData.imgsrc} />
+      <meta property="og:url" content={metaData.url} />
 
       <meta name="twitter:title" content={metaData.title} />
       <meta name="twitter:description" content={metaData.description} />
-      {/* <meta name="twitter:image" content={metaData.imgsrc} /> */}
+      <meta name="twitter:image" content={metaData.imgsrc} />
       {/* <link rel="canonical" href={metaData.url} /> */}
     </Helmet>
   );
