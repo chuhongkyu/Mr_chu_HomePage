b03bd01e1464ce6799fbcc3fd1caaa68e8b03484
Mr.chu <chuhongkyu@gmail.com>
2022-08-20 18:30:11 +0900

    리액트 렌더링

--- src/sitemapGenerator.js
+++ src/sitemapGenerator.js
@@ -2,13 +2,13 @@ require("babel-register")({
   presets: ["es2015", "react"],
 });
 
-const router = require("./sitemapRoutes").default; // 좀 전에 만든 sitemapRoutes 파일이 있는 경로입니다.
+const router = require("./sitemapRoutes").default; // 좀 전에 만든 sitemapRoutes 파일이 있는 경로
 const Sitemap = require("react-router-sitemap").default;
 
 function generateSitemap() {
   return new Sitemap(router)
-    .build("http://chuhongkyu.github.io/Mr_chu_HomePage") // 도메인 이름으로 변경해주세요.
-    .save("./public/sitemap.xml"); // sitemap.xml 파일이 생성될 위치입니다.
+    .build("http://chuhongkyu.github.io/Mr_chu_HomePage") // 도메인
+    .save("./public/sitemap.xml"); // sitemap.xml 파일이 생성될 위치
 }
 
 generateSitemap();
