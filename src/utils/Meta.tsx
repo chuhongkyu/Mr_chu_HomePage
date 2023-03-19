ce984029943808405f89793dc19f1466590ef9a7
Mr.chu <chuhongkyu@gmail.com>
2023-03-19 23:08:29 +0900

    Add description

--- src/utils/Meta.tsx
+++ src/utils/Meta.tsx
@@ -5,7 +5,7 @@ import { Helmet } from "react-helmet-async";
 const Meta = () => {
   const metaData = {
     title: "Mr.chu 홈페이지",
-    description: "추홍규 | 프론트 엔드 · 개발자 · 포트폴리오",
+    description: "추홍규 | 프론트 엔드의 홈페이지입니다. 한국화 전공에서 개발자로 전향한 그의 이력을 확인 할 수 있습니다. 컴퓨터 화면과 같은 홈페이지를 감상해 보세요.",
     keywords: "추홍규 · 프론트 엔드 · Mr.chu · 포트폴리오 · chu_hong_kyu ",
     imgsrc:
       "https://raw.githubusercontent.com/chuhongkyu/Mr_chu_HomePage/main/public/assets/home.png",
