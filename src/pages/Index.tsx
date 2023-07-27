8c0201c307adbcd10544aefa65f588503474d354
Mr.chu <chuhongkyu@gmail.com>
2023-07-27 20:10:56 +0900

    Add index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,12 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  font-family: sans-serif;
+`;
 
 const Index = () => {
   const navigate = useNavigate();
@@ -9,7 +16,7 @@ const Index = () => {
   }, []);
 
   return (
-      <div></div>
+      <Wrapper></Wrapper>
   );
 };
 
