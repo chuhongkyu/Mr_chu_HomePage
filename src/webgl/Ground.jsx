a7f934d5f0991302358156fc0dc49cbd416ed4eb
Mr.chu <chuhongkyu@gmail.com>
2023-11-09 23:08:28 +0900

    Add mr_chu

--- src/webgl/Ground.jsx
+++ src/webgl/Ground.jsx
@@ -1,13 +1,17 @@
 import { usePlane } from "@react-three/cannon";
 import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Grond = ()=> {
+    const isMobile = useMediaQuery({
+        query: '(min-width: 681px)'
+    })
     const [ref] = usePlane(
         () => ({ 
           type: 'Static', 
           material: 'ground',
           rotation: [-Math.PI / 2, 0, 0],
-          position: [0, 1, 0]
+          position: isMobile ? [0, 1.5, 0] : [0, 0.9, 0]
         }), 
         useRef(null)
     );
