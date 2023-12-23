7b35a2615b4631b865b159585656123a10b3161e
Mr.chu <chuhongkyu@gmail.com>
2023-12-23 15:24:53 +0900

    Delete Hdr

--- src/webgl/Scene.jsx
+++ src/webgl/Scene.jsx
@@ -1,11 +1,9 @@
-import { Environment } from "@react-three/drei";
 import { TextGroup } from "./TextGroup";
 import Ground from './Ground';
 
 function Scene() {
   return (
     <>
-        <Environment preset="forest"/>
         <TextGroup/>
         <Ground/>
     </>
