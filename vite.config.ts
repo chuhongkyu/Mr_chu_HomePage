1b8cb2af12b46cd540e5c2417ce33f898fedbb9f
Mr.chu <chuhongkyu@gmail.com>
2024-03-01 01:30:42 +0900

    Test build

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,24 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/three')) {
+            return;
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+        // manualChunks: (id) => {
+        //   if (id.includes("node_modules")) {
+        //     const moduleName = id.split("node_modules/")?.pop()?.split("/")[0];
+        //     return `vendor/${moduleName}`;
+        //   }
+        // },
+      },
+    },
+  }
 })
