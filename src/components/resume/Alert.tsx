65dec30bd7abb14996acda44ecdd77a762f3dde5
unknown <chuhongkyu@gmail.com>
2025-01-10 17:53:37 +0900

    Update ClipBoard Motion

--- src/components/resume/Alert.tsx
+++ src/components/resume/Alert.tsx
@@ -2,44 +2,55 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 
 const Container = styled(motion.div)`
-  padding: 15px 20px;
-  border-radius: 5px;
-  background-color: white;
+  padding: 8px 14px;
+  border-radius: 16px;
+  background-color: #f7f7f7;
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  position: absolute;
+  bottom: 100%;
+  left: -10px;
+  z-index: 12;
   display: flex;
   justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  position: absolute;
-  right: -70px;
-  top: 20%;
-  z-index: 12;
-  div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
   img {
-    width: 30px;
-    height: 30px;
+    display: block;
+    width: 22px;
+    height: 22px;
+    object-fit: cover;
     margin-right: 5px;
   }
   p {
     white-space: nowrap;
+    font-size: 14px;
   }
 `;
 
 const Alert = () => {
   return (
-    <Container
-      key="modal"
-      animate={{ scale: [0, 1], transition: { duration: 0.5 } }}
-      exit={{ scale: 0 }}
-    >
-      <div>
-        <img src={"/assets/img/memo.png"} alt="memo" />
-        <p>이메일 복사 되었습니다.</p>
-      </div>
+      <Container
+        key="modal"
+        initial={{ opacity: 0, y: 20, scale: 0.9 }}
+        animate={{
+          opacity: 1,
+          y: 0,
+          scale: 1,
+          transition: {
+            duration: 0.4,
+            ease: [0.22, 1, 0.36, 1],
+          },
+        }}
+        exit={{
+          opacity: 0,
+          y: 20,
+          scale: 0.9,
+          transition: {
+            duration: 0.4,
+            ease: [0.22, 1, 0.36, 1],
+          },
+        }}
+      >
+      <img src={"/assets/img/memo.png"} alt="memo" />
+      <p>Copied to clipboard</p>
     </Container>
   );
 };
